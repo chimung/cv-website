e9785a7b17774a989a09713090be919112efe85b
hungcung <cungthehung@gmail.com>
2022-09-17 23:24:13 +0700

    setup layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainBodyComponent } from './layout/main-body/main-body.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainBodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
